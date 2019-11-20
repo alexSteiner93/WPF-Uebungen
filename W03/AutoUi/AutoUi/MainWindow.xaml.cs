0979df6ef3dbc5fc68edcf98f1a96f40a296d680
Martin Seelhofer <ms@odddots.ch>
2019-11-20 11:37:59 +0100

    Inhalt der Projektvorlagen richtiggestellt (war gerade verkehrt herum AutoUi vs. ModernUi)

--- W03/AutoUi/AutoUi/MainWindow.xaml.cs
+++ W03/AutoUi/AutoUi/MainWindow.xaml.cs
@@ -13,56 +13,14 @@ namespace ModernUi
             InitializeComponent();            
         }
 
-        private void TitlePanel_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void AutoWindow_Click(object sender, RoutedEventArgs e)
         {
-            DragMove();
+            new AutoWindow().Show();
         }
 
-        private void CloseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        private void CustomerWindow_Click(object sender, RoutedEventArgs e)
         {
-            e.CanExecute = true;
-        }
-
-        private void MaximizeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = WindowState != WindowState.Maximized;
-        }
-
-        private void MinimizeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = WindowState != WindowState.Minimized;
-        }
-
-        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.CloseWindow(this);
-        }
-
-        private void MaximizeCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.MaximizeWindow(this);
-        }
-
-        private void MinimizeCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.MinimizeWindow(this);
-        }
-
-        private void TitlePanel_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2 && WindowState == WindowState.Maximized)
-            {
-                SystemCommands.RestoreWindow(this);
-            }
-            else if (e.ClickCount == 2 && WindowState == WindowState.Normal)
-            {
-                SystemCommands.MaximizeWindow(this);
-            }
-        }
-
-        private void QuitButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            SystemCommands.CloseWindow(this);
+            new CustomerWindow().Show();
         }
     }
 }
--- W03/ModernUi/ModernUi/MainWindow.xaml.cs
+++ W03/ModernUi/ModernUi/MainWindow.xaml.cs
@@ -13,14 +13,56 @@ namespace ModernUi
             InitializeComponent();            
         }
 
-        private void AutoWindow_Click(object sender, RoutedEventArgs e)
+        private void TitlePanel_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            new AutoWindow().Show();
+            DragMove();
         }
 
-        private void CustomerWindow_Click(object sender, RoutedEventArgs e)
+        private void CloseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            new CustomerWindow().Show();
+            e.CanExecute = true;
+        }
+
+        private void MaximizeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = WindowState != WindowState.Maximized;
+        }
+
+        private void MinimizeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = WindowState != WindowState.Minimized;
+        }
+
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void MaximizeCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void MinimizeCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void TitlePanel_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && WindowState == WindowState.Maximized)
+            {
+                SystemCommands.RestoreWindow(this);
+            }
+            else if (e.ClickCount == 2 && WindowState == WindowState.Normal)
+            {
+                SystemCommands.MaximizeWindow(this);
+            }
+        }
+
+        private void QuitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
         }
     }
 }
