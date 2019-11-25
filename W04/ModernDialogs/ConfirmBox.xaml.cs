3b0da369e99ce585f3287c31923f7c5819006f99
omd <oldmacdonald@bluewin.ch>
2019-11-25 22:45:49 +0100

    Projektvorlage für W04 - Aufgabe 1 ergänzt

---
+++ W04/ModernDialogs/ConfirmBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ModernDialogs
+{
+    /// <summary>
+    /// Interaction logic for ConfirmBox.xaml
+    /// </summary>
+    public partial class ConfirmBox : Window
+    {
+        public ConfirmBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
